a4cfc712b703377f4a2348a2964ba9e0ccb9cc72
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-10-12 11:50:17 +0300

    Create airbnb_curl.sh

---
+++ src/airbnb_curl.sh
@@ -0,0 +1,4 @@
+curl --user USERNAME:PASSWORD \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.airbnb.com/?tab_id=home_tab&refinement_paths%5B%5D=/homes&search_mode=flex_destinations_search&flexible_trip_lengths%5B%5D=one_week&location_search=MIN_MAP_BOUNDS&monthly_start_date=2023-07-01&monthly_length=3&price_filter_input_type=0&price_filter_num_nights=5&channel=EXPLORE&search_type=category_change&category_tag=Tag:8522", "geo_location": "United States", "render": "html"}'
